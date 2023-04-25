79bdd0dee0b7471196ca5dca690eaeb9ab8c5919
Mohamed Fazil M <fazilmm860@gmail.com>
2023-04-25 18:08:07 +0530

    Final Commit

---
+++ script.js
@@ -0,0 +1,36 @@
+let countdown = document.getElementById("countdown");
+
+setTimeout(() => {
+    countdown.textContent = '10';
+    setTimeout(() => {
+        countdown.textContent = '9';
+        setTimeout(() => {
+            countdown.textContent = '8';
+            setTimeout(() => {
+                countdown.textContent = '7';
+                setTimeout(() => {
+                    countdown.textContent = '6';
+                    setTimeout(() => {
+                        countdown.textContent = '5';
+                        setTimeout(() => {
+                            countdown.textContent = '4';
+                            setTimeout(() => {
+                                countdown.textContent = '3';
+                                setTimeout(() => {
+                                    countdown.textContent = '2';
+                                    setTimeout(() => {
+                                        countdown.textContent = '1';
+                                        setTimeout(() => {
+                                            countdown.textContent = "HAPPY INDEPENDENCE DAY"
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+
+        }, 1000)
+    }, 1000)
+}, 1000)
